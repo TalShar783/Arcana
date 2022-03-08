99ab4b63d4221b815b95d364a5d17a4a7d266034
TalShar783 <nacustis@gmail.com>
2022-03-08 11:51:34 -0500

    Added a lot of formatting. Currently stable and usable.

--- src/arcana_bot.py
+++ src/arcana_bot.py
@@ -14,12 +14,30 @@ module.
 There are a number of utility commands being showcased here.'''
 
 intents = discord.Intents.all()
-intents.members = True
 
 thisGame = game.GameClass()
-fullDeck = deck.DeckClass()
 
 bot = commands.Bot(command_prefix='/', description=description, intents=intents)
+
+def sendEmbed(title: str, url: str, description: str, color):
+    embed = discord.Embed(title=title, url=url, description=description, color=color)
+    return embed
+
+def houseColor(house):
+    switcher = {
+        # "Darkness": "#9c0399",
+        # "Light": "#fdff8f",
+        # "Fire": "#ff2f00",
+        # "Ice": "#00f7ff",
+        # "Unaligned": "#a1a1a1"
+        "Darkness": discord.Colour.purple(),
+        "Light": discord.Colour.yellow(),
+        "Fire": discord.Colour.red(),
+        "Ice": discord.Colour.blue(),
+        "Unaligned": discord.Colour.dark_gray()
+    }
+    return switcher.get(house)
+
 
 @bot.event
 async def on_ready():
@@ -34,17 +52,19 @@ async def reset(ctx):
     print("The game has been reset!")
     print(f"Players: {thisGame.players}")
     print(f"Cards remaining in deck: {thisGame.deck.cardsRemaining()}")
+    await ctx.send("The game has been reset.")
 
 
 @bot.command()
-async def draw(ctx, name: str):
-    print(f"Drawing a card for {name}...")
-    if thisGame == None:
-        thisGame.reset()
-    thisGame.addPlayer(name)
-    for p in thisGame.players:
-        if name == p.name:
-            p.draw(thisGame.deck)
+async def draw(ctx, name: str, number: int = 1):
+    for i in range(number):
+        print(f"Drawing a card for {name}...")
+        thisGame.addPlayer(name)
+        for p in thisGame.players:
+            if name == p.name:
+                drawnCard = p.draw(thisGame.deck)
+                color = houseColor(drawnCard.getHouse())
+                await ctx.send(embed=sendEmbed(f"**{p.name}\n\n {drawnCard.show()}**", "", drawnCard.explain(), color))
     return
 
 @bot.command()
@@ -55,40 +75,42 @@ async def showHand(ctx, player: str):
     for p in thisGame.players:
         if player in p.name:
             for c in p.hand:
-                output += f"\n{c.show()}"
+                output += f"\n**{c.show()}**"
+                color = houseColor(c.getHouse())
+                await ctx.send(embed=sendEmbed(f"**{p.name}\n\n {c.show()}**", "", "", color))
             print(output)
-            await ctx.send(output)
+
 
 @bot.command()
 async def explainHand(ctx, player: str):
     print(f"{ctx.author} has requested to see {player}'s cards and their meaning.")
     output: str = ""
-    output += f"{player}: "
+    output += f"**{player}:** "
     for p in thisGame.players:
         if player in p.name:
             for c in p.hand:
-                output += f"\n{c.show()}: {c.explain()}"
+                color = houseColor(c.getHouse())
+                output += f"\n\n**{c.show()}:** {c.explain()}"
+                await ctx.send(embed=sendEmbed(f"**{p.name}\n\n {c.show()}**", "", c.explain(), color))
             print(output)
-            await ctx.send(output)
 
 @bot.command()
 async def explain(ctx, *args: str):
     card = " ".join(args)
     print(f"{ctx.author} has requested an explanation of the card {card}.")
     output: str = ""
-    for c in fullDeck.cards:
+    for c in thisGame.deck.cards:
         if card in c.n:
-            output += f"{c.show()}: {c.explain()}"
-    print(output)
-    try:
-        await ctx.send(output)
-    except discord.errors.HTTPException:
-        number = random.randint(1,10)
-        match number:
-            case 10:
-                await ctx.send("How about go fuck yourself.")
-            case _:
-                await ctx.send("Oops, that's not the proper name of a card. Try again!")
+            color = houseColor(c.getHouse())
+            try:
+                await ctx.send(embed=sendEmbed(f"**{c.show()}**", "", c.explain(), color))
+            except discord.errors.HTTPException:
+                number = random.randint(1,10)
+                match number:
+                    case 10: await ctx.send("How about go fuck yourself.")
+                    case _: await ctx.send("Oops, that's not the proper name of a card. Try again!")
+
+
 
 @bot.command()
 async def roll(ctx, dice: str):
